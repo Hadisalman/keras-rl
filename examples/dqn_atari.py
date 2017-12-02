641b6f88a1236402fca3fa35cf14771098e3bbb0
hadisalman <hadicsalman@gmail.com>
2017-12-01 20:56:41 -0500

    Added tensorboard loggers

--- examples/dqn_atari.py
+++ examples/dqn_atari.py
@@ -16,7 +16,7 @@ from rl.agents.dqn import DQNAgent
 from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 from rl.core import Processor
-from rl.callbacks import FileLogger, ModelIntervalCheckpoint
+from rl.callbacks import FileLogger, ModelIntervalCheckpoint, tensorboardLogger
 
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
@@ -30,25 +30,26 @@ K.set_session(sess)
 INPUT_SHAPE = (84, 84)
 WINDOW_LENGTH = 4
 
+log_parent_dir = './train_log'
 
 class AtariProcessor(Processor):
-    def process_observation(self, observation):
-        assert observation.ndim == 3  # (height, width, channel)
-        img = Image.fromarray(observation)
-        img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
-        processed_observation = np.array(img)
-        assert processed_observation.shape == INPUT_SHAPE
-        return processed_observation.astype('uint8')  # saves storage in experience memory
+	def process_observation(self, observation):
+		assert observation.ndim == 3  # (height, width, channel)
+		img = Image.fromarray(observation)
+		img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
+		processed_observation = np.array(img)
+		assert processed_observation.shape == INPUT_SHAPE
+		return processed_observation.astype('uint8')  # saves storage in experience memory
 
-    def process_state_batch(self, batch):
-        # We could perform this processing step in `process_observation`. In this case, however,
-        # we would need to store a `float32` array instead, which is 4x more memory intensive than
-        # an `uint8` array. This matters if we store 1M observations.
-        processed_batch = batch.astype('float32') / 255.
-        return processed_batch
+	def process_state_batch(self, batch):
+		# We could perform this processing step in `process_observation`. In this case, however,
+		# we would need to store a `float32` array instead, which is 4x more memory intensive than
+		# an `uint8` array. This matters if we store 1M observations.
+		processed_batch = batch.astype('float32') / 255.
+		return processed_batch
 
-    def process_reward(self, reward):
-        return np.clip(reward, -1., 1.)
+	def process_reward(self, reward):
+		return np.clip(reward, -1., 1.)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', choices=['train', 'test'], default='train')
@@ -66,13 +67,13 @@ nb_actions = env.action_space.n
 input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 model = Sequential()
 if K.image_dim_ordering() == 'tf':
-    # (width, height, channels)
-    model.add(Permute((2, 3, 1), input_shape=input_shape))
+	# (width, height, channels)
+	model.add(Permute((2, 3, 1), input_shape=input_shape))
 elif K.image_dim_ordering() == 'th':
-    # (channels, width, height)
-    model.add(Permute((1, 2, 3), input_shape=input_shape))
+	# (channels, width, height)
+	model.add(Permute((1, 2, 3), input_shape=input_shape))
 else:
-    raise RuntimeError('Unknown image_dim_ordering.')
+	raise RuntimeError('Unknown image_dim_ordering.')
 model.add(Convolution2D(32, 8, 8, subsample=(4, 4)))
 model.add(Activation('relu'))
 model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
@@ -97,7 +98,7 @@ processor = AtariProcessor()
 # (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
 # so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=1000000)
+							  nb_steps=1000000)
 
 # The trade-off between exploration and exploitation is difficult and an on-going research topic.
 # If you want, you can experiment with the parameters or use a different policy. Another popular one
@@ -106,31 +107,55 @@ policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., valu
 # Feel free to give it a try!
 
 dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-               processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-               train_interval=4, delta_clip=1.)
+			   processor=processor, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
+			   train_interval=4, delta_clip=1.)
 dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
+def make_log_dir():
+	import datetime, os
+	current_timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+	log_dir = os.path.join(log_parent_dir, args.env_name, current_timestamp)
+	os.makedirs(log_dir)
+	os.makedirs(os.path.join(log_dir, 'weights'))
+	os.makedirs(os.path.join(log_dir, 'replay_memory'))
+	os.makedirs(os.path.join(log_dir, 'gym_monitor'))
+	# create empty logfiles now
+	# log_files = {
+	# 					'train_loss': os.path.join(lod_dir, 'train_loss.txt'),
+	# 					'train_episode_reward': os.path.join(lod_dir, 'train_episode_reward.txt'),
+	# 					'test_episode_reward': os.path.join(lod_dir, 'test_episode_reward.txt')
+	# 				  }
+	# for key in self.log_files:
+	# 	open(os.path.join(self.log_dir, self.log_files[key]), 'a').close()
+log_dir=''
 if args.mode == 'train':
-    # Okay, now it's time to learn something! We capture the interrupt exception so that training
-    # can be prematurely aborted. Notice that you can the built-in Keras callbacks!
-    weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
-    checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
-    log_filename = 'dqn_{}_log.json'.format(args.env_name)
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
-    callbacks += [FileLogger(log_filename, interval=100)]
-
-    #weights_filename = args.weights
-    #dqn.load_weights(weights_filename)    
-    dqn.fit(env, callbacks=callbacks, nb_steps=3000000, log_interval=10000)
-
-    # After training is done, we save the final weights one more time.
-    dqn.save_weights(weights_filename, overwrite=True)
+	make_log_dir()
+	# Okay, now it's time to learn something! We capture the interrupt exception so that training
+	# can be prematurely aborted. Notice that you can the built-in Keras callbacks!
+	weights_filename =os.path.join(log_dir, 'weights','dqn_{}_weights.h5f'.format(args.env_name))
+	checkpoint_weights_filename = os.path.join(log_dir, '{step}.h5f')
+	callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
 	
-    # Finally, evaluate our algorithm for 10 episodes.
-    dqn.test(env, nb_episodes=10, visualize=False)
+	# log_filename = 'dqn_{}_log.json'.format(args.env_name)
+	# callbacks += [FileLogger(log_filename, interval=100)]
+	
+	callbacks += [tensorboardLogger(log_dir)]
+
+	#weights_filename = args.weights
+	#dqn.load_weights(weights_filename)    
+	dqn.fit(env, callbacks=callbacks, nb_steps=3000000, log_interval=10000, verbose=1)
+
+	# After training is done, we save the final weights one more time.
+	dqn.save_weights(weights_filename, overwrite=True)
+	
+	# Finally, evaluate our algorithm for 10 episodes.
+	dqn.test(env, nb_episodes=10, visualize=False)
+
+
 elif args.mode == 'test':
-    weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
-    if args.weights:
-        weights_filename = args.weights
-    dqn.load_weights(weights_filename)
-    dqn.test(env, nb_episodes=10, visualize=True)
+
+	weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
+	if args.weights:
+		weights_filename = args.weights
+	dqn.load_weights(weights_filename)
+	dqn.test(env, nb_episodes=10, visualize=True)
